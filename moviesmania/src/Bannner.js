59f70e0175278b8d3b51cad89c63760649b7d850
Chhandak Roy <chhandak@Chhandaks-MacBook-Air.local>
2023-07-19 23:23:41 -0400

    Progress1.0

--- moviesmania/src/Bannner.js
+++ moviesmania/src/Bannner.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import './Banner.css'
 function Bannner() {
+    function shortner(string, n) {
+        if(string.length > n ) {
+            return string.substr(0,n-1) + '..';
+        }
+        return string;
+    }
     return (
-    <div className='banner'>
-        
-    </div>
+    <header className='banner' 
+        style={{
+        backgroundSize: 'cover',
+        backgroundImage: `url('https://pixy.org/src/49/491517.png')`,
+        backgroundPosition: "center center",
+    }}>
+        <div className='banner__contents'>
+            <h1 className='banner__title'> Movie Name</h1>
+            <div className='bannner__buttons'>
+                <button className='banner__button'>Play</button>
+                <button className='bann er__button'>My List</button>
+            </div>
+            <h1 className='banner__description'>
+                {shortner(`Test Description`,3)}
+            </h1>
+            <div className='banner--fadebutton' />
+        </div>
+    </header>
     )
 }
 
--- moviesmania/src/HomeScreen.js
+++ moviesmania/src/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./HomeScreen.css"
 import Nav from './Nav'
+import Bannner from './Bannner'
 function HomeScreen() {
     return (
     <div className='homeScreen'>
         <Nav />
+        <Bannner />
     </div>
     )
 }
